1d00e09135a1ac3819d81552586cab194da1634d
ganeshkumar222 <59387877+ganeshkumar222@users.noreply.github.com>
2020-02-05 00:02:35 +0530

    Add files via upload

---
+++ src/Components/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Albumwrapper from './Albumwrapper';
+import { Route, Switch } from 'react-router-dom'
+import Imageviewer from './Imageviewer'
+class App extends Component {
+  constructor() {
+    super();
+    this.name = "Jack";
+    this.state = {
+      title: ''
+    };
+  }
+  callbackFunction = (childData) => {
+    this.setState({ title: childData })
+  }
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={() => <Albumwrapper cab={this.callbackFunction}></Albumwrapper>} />
+          <Route exact path="/thumbs" component={() => <Imageviewer albumtitle={this.state.title} />}></Route>
+        </Switch>
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/Tests/Imageviewer.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Imageviewer from "./Imageviewer"
+import { shallow } from 'enzyme';
+describe("Imageviewer", () => {
+  it("rendering of component", () => {
+    const wrapper = shallow(<Imageviewer />);
+  });
+  it("checking for props of a component", () => {
+    const wrapper = shallow(<Imageviewer someProp={1} />);
+    expect(wrapper.props().someProp).toEqual(1);
+
+  });
+  it("checking for state component", () => {
+    const wrapper = shallow(<Imageviewer />);
+    wrapper.setState({ thumbnail: false });
+    expect(wrapper.state().fullimage).toBeDefined();
+  });
+});
